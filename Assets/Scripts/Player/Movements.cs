75993f1c2606a36ccecc3b422f1037516c57de80
elfabri <111920054+elfabri@users.noreply.github.com>
2025-08-08 23:47:41 -0300

    rotation & move by force, using PlayerInput system

--- Assets/Scripts/Player/Movements.cs
+++ Assets/Scripts/Player/Movements.cs
@@ -5,18 +5,39 @@ public class Movements : MonoBehaviour
 {
 
     private Rigidbody _rb;
-    public PlayerInput playerInput;
-    private Vector3 rawInputMovement;
+    private PlayerInput _playerInput;
+
+    private Vector3 inputVector;
+    private Vector3 accelerationVector;
+
+    public float movementSpeed = 5f;
+    public float turnSpeed = 100f;
+    private float turnAmount = 0f;
+    private Quaternion turnOffset;
 
     void Awake()
     {
+        _playerInput = GetComponent<PlayerInput>();
+        InputSystem.actions.Disable();
+        _playerInput.currentActionMap?.Enable();
+
         _rb = GetComponent<Rigidbody>();
     }
 
     public void OnMove(InputValue value)
     {
-        Vector2 inputMovement = value.Get<Vector2>();
-        rawInputMovement = new Vector3(inputMovement.x, 0, inputMovement.y);
-        _rb.MovePosition(transform.position + rawInputMovement);
+        inputVector = value.Get<Vector2>();
+
+        // correction to 3D
+        accelerationVector = transform.forward * inputVector.y;
+
+        turnAmount = inputVector.x * turnSpeed * Time.fixedDeltaTime;
+        turnOffset = Quaternion.Euler( 0, turnAmount, 0 );
+    }
+
+    void Update()
+    {
+        _rb.AddForce(accelerationVector * movementSpeed, ForceMode.Acceleration);
+        _rb.MoveRotation(Quaternion.Normalize(_rb.rotation * turnOffset));
     }
 }
